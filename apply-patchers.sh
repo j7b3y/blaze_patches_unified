893140e6aaaf747afc40f6bc712139088771b429
j7b3y <Glowing3386@proton.me>
2022-08-15 10:21:40 +0900

    test

--- apply-patchers.sh
+++ apply-patchers.sh
@@ -3,28 +3,19 @@
 set -e
 
 patches="$(readlink -f -- $1)"
+tree="$2"
+branches="$3 common"
 
-shopt -s nullglob
-for project in $(cd $patches; echo *);do
-	p="$(tr _ / <<<$project |sed -e 's;platform/;;g')"
-	[ "$p" == build ] && p=build/make
-	[ "$p" == vendor/hardware/overlay ] && p=vendor/hardware_overlay
-	[ "$p" == vendor/partner/gms ] && p=vendor/partner_gms
-	pushd $p
-	git clean -fdx; git reset --hard
-	for patch in $patches/*/$project/*.patch;do
-		if git apply --check $patch;then
-			git am $patch
-		elif patch -f -p1 --dry-run < $patch > /dev/null;then
-			#This will fail
-			git am $patch || true
-			patch -f -p1 < $patch
-			git add -u
-			git am --continue
-		else
-			echo "Failed applying $patch"
-			exit 1
-		fi
-	done
-	popd
+for project in $patches/*; do
+    for project in $(cd $patches/*/$project; echo *); do
+        p="$(tr _ / <<<$project |sed -e 's;platform/;;g')"
+        [ "$p" == build ] && p=build/make
+        [ "$p" == treble/app ] && p=treble_app
+        [ "$p" == vendor/hardware/overlay ] && p=vendor/hardware_overlay
+        pushd $p &>/dev/null
+        for patch in $patches/patches/$tree/$b/$project/*.patch; do
+            git am $patch || exit
+        done
+        popd &>/dev/null
+    done
 done
